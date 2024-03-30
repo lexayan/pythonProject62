6cbd6d9bb21680fc6a25229b85f4d849b26d73dd
100Ballov <f@100bllov.by>
2024-03-30 15:29:59 +0300

    3

--- GAME/main.py
+++ GAME/main.py
@@ -3,12 +3,24 @@ import pygame as pg
 
 pg.init()
 
+#Config Variables
+W = 1280
+H = 720
+FPS = 60
+clock = pg.time.Clock()
 
 
+screen = pg.display.set_mode((W, H))
+pg.display.set_caption('Catch an apple')
 
+game_over = False
 
-
-
+while not game_over:
+    clock.tick(FPS)
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
 
 
 
