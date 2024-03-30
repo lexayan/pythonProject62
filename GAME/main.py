6428d4709b1129b98eb7508481a006eede9b7bf2
100Ballov <f@100bllov.by>
2024-03-30 15:55:14 +0300

    4

--- GAME/main.py
+++ GAME/main.py
@@ -1,6 +1,6 @@
 import sys
 import pygame as pg
-
+import random as r
 pg.init()
 
 #Config Variables
@@ -8,6 +8,21 @@ W = 1280
 H = 720
 FPS = 60
 clock = pg.time.Clock()
+
+
+
+#COLORS
+GRAY = (79, 74, 71)
+RED = (219, 20, 20)
+BROWN = (150, 86, 41)
+
+
+#GAME OBJECTS
+player = pg.Rect(0, H-30, 100, 20)
+player.centerx = W//2
+apple = pg.Rect(r.randint(40, W - 40), -50, 40, 40)
+
+
 
 
 screen = pg.display.set_mode((W, H))
@@ -22,5 +37,14 @@ while not game_over:
             pg.quit()
             sys.exit()
 
+    screen.fill(GRAY)
+    pg.draw.rect(screen, BROWN, player)
+    pg.draw.ellipse(screen, RED, apple)
 
 
+    pg.display.update()
+
+    apple.y += 5
+    if apple.bottom > H:
+        apple.x = r.randint(40, W-40)
+        apple.y = -50
